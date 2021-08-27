ee301f104d57bcab8d7518c897853cb144b4c374
mzbhewtee <abimbolaikus@gmail>
2021-08-27 20:18:27 +0200

    alphabeth

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - prints the last digit of the random
+* number stored in the variable n
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z', ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
