5f23f165d7ac64f690868526988ef3274227b4ed
mzbhewtee <abimbolaikus@gmail>
2021-08-31 06:24:27 +0200

    loops

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Program that prints holberton.c
+* followed by a new line
+* return value 0
+*/
+
+int main()
+{
+char ch = 'Holberton\n';
+
+putchar(ch);
+
+return (0);
+
+}
