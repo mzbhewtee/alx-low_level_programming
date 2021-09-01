21e157fe1996e8e063a710470957b4f25600bde9
mzbhewtee <abimbolaikus@gmail>
2021-09-01 16:55:16 +0200

    loops esdd

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
-* print_alphabet - prints the alphabet, in lowercase, followed by a new line
+* print_alphabet_x10 - prints the alphabet, in lowercase, followed by a new line
+* Return: Always 0.
 */
 void print_alphabet_x10(void)
 {
