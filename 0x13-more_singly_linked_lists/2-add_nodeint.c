047d0167372d583571799bac5adb8a73d0a4f64c
Ikudehinbu Beauty <86835129+mzbhewtee@users.noreply.github.com>
2021-10-12 19:12:45 +0200

    Update 2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -8,15 +8,15 @@
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-listint_t *new_node;
+listint_t *new;
 
-new_node = malloc(sizeof(listint_t));
+new = malloc(sizeof(listint_t));
 if (!new)
 return (NULL);
 
-new_node->n = n;
-new_node->next = *head;
-*head = new_node;
+new->n = n;
+new->next = *head;
+*head = new;
 
-return (new_node);
+return (new);
 }
