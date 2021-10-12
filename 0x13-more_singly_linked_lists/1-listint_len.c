3381780d28333253bf50494b4168411953d1edc0
Ikudehinbu Beauty <86835129+mzbhewtee@users.noreply.github.com>
2021-10-12 17:35:14 +0200

    Create 1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements in
+ * a linked list.
+ * @h: head of a list.
+ * Return: numbers of nodes.
+ */
+size_t listint_len(const listint_t *h)
+{
+size_t num_nodes = 0;
+
+while (h)
+{
+
+h = h->next;
+num_nodes++;
+}
+return (num_nodes);
+}
