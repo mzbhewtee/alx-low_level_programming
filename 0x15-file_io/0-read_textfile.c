78ccd0cb7c0e2a3df1ab8e7b7208e8c0ea22ca2f
Ikudehinbu Beauty <86835129+mzbhewtee@users.noreply.github.com>
2021-10-19 02:20:45 +0200

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,38 +10,28 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int fd;
-int s, t;
-char *buf;
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
 
-if (!filename)
-return (0);
+	if (!filename)
+		return (0);
 
-fd = open(filename, O_RDONLY);
-if (fd < 0)
-return (0);
+	fd = open(filename, O_RDONLY);
 
-buf = malloc(sizeof(char) * letters);
-if (!buf)
-return (0);
+	if (fd == -1)
+		return (0);
 
-s = read(fd, buf, letters);
-if (s < 0)
-{
-free(buf);
-return (0);
-}
-buf[s] = '\0';
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
 
-close(fd);
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
 
-t = write(STDOUT_FILENO, buf, s);
-if (t < 0)
-{
-free(buf);
-return (0);
-}
+	close(fd);
 
-free(buf);
-return (t);
+	free(buf);
+
+	return (nwr);
 }
